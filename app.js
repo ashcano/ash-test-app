84c2e6ac6d55d83fc4a556cb86a9b64199e19ac3
AshCano <ashley.cano@salesforce.com>
2022-06-08 16:51:06 -0500

    corrected port bug

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = new App({
 
 (async () => {
   // Start your app
-  await app.start(process.env.PORT || 3000);
+  await app.start(Number(process.env.PORT || 3000));
 
   console.log('⚡️ Bolt app is running!');
 })();
